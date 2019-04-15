cfd465c266c3abb654fce5fd7a35772362f405a7
AliMarashian <ali.marashian@gmail.com>
2019-04-15 13:33:06 +0430

    the very basis

---
+++ src/Model.java
@@ -0,0 +1,22 @@
+public class Model {
+    class Card{}
+    class MovableCard extends Card{}
+    class Hero extends MovableCard{}
+    class Minion extends MovableCard{}
+    class Spell extends Card{}
+    class Impact{}
+    class Player{}
+    class Item{}
+    class InfluentialItem extends Item{}
+    class Flag extends Item{}
+    class CollectibleItem extends InfluentialItem{}
+    class UsableItem extends InfluentialItem{}
+    class Collection{}
+    class Shop{}
+    class Deck{}
+    class Hand{}
+    class Match{}
+    class Coordination{}
+    class Cell{}
+    class Table{}
+}
