6aab80de37e0c306f3e305fa3199093b1a12e922
AliMarashian <ali.marashian@gmail.com>
2019-05-06 01:14:28 +0430

    debugging

--- src/model/Minion.java
+++ src/model/Minion.java
@@ -33,7 +33,14 @@ public class Minion extends MovableCard {
         minion.onDefendImpact = onDefendImpact == null ? null : onDefendImpact.copy();
         minion.onAttackImpact = onAttackImpact == null ? null : onAttackImpact.copy();
         minion.collectionID = this.collectionID;
-        this.setCardfieldsForCopy(minion);
+        minion.manaCost = manaCost;
+        minion.name = name;
+        minion.cell = cell;
+        minion.cost = cost;
+        minion.match = match;
+        minion.player = player;
+        minion.cardID = cardID;
+        minion.description = description;
         return minion;
     }
 
