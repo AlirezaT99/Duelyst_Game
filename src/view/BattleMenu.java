75b93ebbb6315433791317a60476d58e5c48bbf2
sepehrAmini <sepehramini83@gmail.com>
2019-05-07 16:19:11 +0700

    hjkhkh

--- src/view/BattleMenu.java
+++ src/view/BattleMenu.java
@@ -119,6 +119,8 @@ public class BattleMenu {
                             break;
                     }
                 }
+            }catch (Throwable e){
+                e.printStackTrace();
             }
         }
         scanner.close();
