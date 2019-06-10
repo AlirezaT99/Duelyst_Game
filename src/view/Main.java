5e6e9d9e0c6edcdb9ff0aae506a9a55455c16284
AliMarashian <ali.marashian@gmail.com>
2019-06-10 12:21:27 +0430

    main somehow there now

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,12 +1,36 @@
 package view;
 
+import com.sun.scenario.effect.impl.prism.PrImage;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
 import java.io.IOException;
 
-public class Main {
+public class Main  extends Application {
     private static LoginMenu loginMenu = new LoginMenu();
 
-    public static void main(String[] args) throws IOException {
+    public static void main (String[] args) throws IOException {
             presenter.MainProcess.readFiles();
+            launch(args);
             loginMenu.run();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Login login = new Login();
+        Scene currentScene = login.start(primaryStage);
+        primaryStage.setScene(currentScene);
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                System.exit(0);
+            }
+        });
+        primaryStage.show();;
+
     }
 }
